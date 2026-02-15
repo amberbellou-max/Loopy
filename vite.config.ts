8b216bb6c0f71355217588b992155da77e4670ac
Abel Sanchez <abelsanchez@users.noreply.github.com>
2026-02-15 11:40:08 -0500

    Add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 export default defineConfig({
+  base: isGithubActions && repoName ? `/${repoName}/` : "/",
   server: {
     port: 5173,
   },
